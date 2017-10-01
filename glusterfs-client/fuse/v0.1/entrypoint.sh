147a2344a71f0668f1b429ae457fe175f8c5eca3
Melwin <melwinkieffer@gmail.com>
2017-10-01 16:04:30 +0200

    add: Umount volume

--- glusterfs-client/fuse/v0.1/entrypoint.sh
+++ glusterfs-client/fuse/v0.1/entrypoint.sh
@@ -5,6 +5,7 @@ dmesg | grep -i fuse
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
 NC='\033[0m' # No Color
 VOL_DIR='/vol'
 
@@ -23,11 +24,22 @@ mkdir -p $VOL_DIR/$GLUSTER_VOLUME
 mount $VOL_DIR/$GLUSTER_VOLUME
 rc=$?
 
+term_handler() {
+    printf "\n${YELLOW}SIGTERM received ${NC}\xE2\x9A\xA1\n"
+    umount $VOL_DIR/$GLUSTER_VOLUME
+    if [[ $? -eq 0 ]]; then
+	printf "\n${GREEN}Unmount succeed ! ${NC}\xF0\x9F\x98\x80 \n\n"
+    else
+	printf "\n${RED}Unount failed ! ${NC}\xF0\x9F\x98\xAD \n\n"
+    fi
+    exit 0
+}
+trap 'kill ${!}; term_handler' SIGTERM
+
 logfile=/var/log/glusterfs$VOL_DIR-$GLUSTER_VOLUME.log
 if [[ $rc -eq 0 ]]; then
     printf "\n${GREEN}Mount succeed ! ${NC}\xF0\x9F\x98\x80 \n\n"
-    tail -F $logfile
-    umount $VOL_DIR/$GLUSTER_VOLUME
+    tail -F $logfile & wait ${!}
 else
     printf "\n${RED}Mount failed ! ${NC}\xF0\x9F\x98\xAD \n\n"
     cat $logfile
